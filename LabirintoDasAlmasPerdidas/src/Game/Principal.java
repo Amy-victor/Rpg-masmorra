c6f24b8b9af1b7a9a45c1df3c64d40da087240d5
Amy <victorhugobaiajunqueira@gmail.com>
2024-11-26 20:42:31 -0300

    atualização rapida

--- LabirintoDasAlmasPerdidas/src/Game/Principal.java
+++ LabirintoDasAlmasPerdidas/src/Game/Principal.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+    public static void carregar(){
         Personagem p1 = new Personagem();
 
         Monstros m1 = new Monstros();
         
 
-    }
-
+        }
+        
 
 }
