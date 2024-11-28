2e9dda1d62807385572888597ed413baa020727e
Amy <victorhugobaiajunqueira@gmail.com>
2024-11-27 23:34:57 -0300

    adicionado metodos de mana e d2

--- LabirintoDasAlmasPerdidas/src/Game/Principal.java
+++ LabirintoDasAlmasPerdidas/src/Game/Principal.java
@@ -1,18 +1,58 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Principal {
-    
-    public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       Personagem p1 = new Personagem();
-       Monstros m1 = new Monstros();
-
-    }   
-
-        }
-
+    //área de carregar as variáveis
+    static Scanner scanner = new Scanner(System.in);
+    static Random rolagem = new Random();
+    static Personagem p1 = new Personagem();
+    static Monstros m1 = new Monstros();
+    static int resposta;
+                @SuppressWarnings("unused")
+                public static void main(String[] args) {
+            
+                }   
+                //procedimento de rolagens
+                public static boolean moeda(){
+                    int i = 1;
+                    boolean acerto = false;
+                    int d2 = 0;
+                    while (i == 1) {
+                    System.out.println("1- Cara");
+                    System.out.println("2- Coroa");
+                    resposta = scanner.nextInt();
+                    switch (resposta) {
+                        case 1:
+                        i = 2;
+                       d2 = rolagem.nextInt(2)+1;
+                        if (d2 == 1) {
+                            System.out.println("Cara!!!");
+                            acerto = true;
+                        }else{
+                            System.out.println("Coroa...");
+                            acerto = false;
+                        }
+                            break;
+                        case 2:
+                        i = 2;
+                        d2 = rolagem.nextInt(2)+1;
+                        if (d2 == 2) {
+                            System.out.println("Coroa!!!");
+                            acerto = true;
+                        }else{
+                            System.out.println("Cara...");
+                            acerto = false;
+                        }
+                        default:
+                        System.out.println("Não há essa escolha.");
+                            break;
+                    }
+                }
+                return acerto;
+                }
+    //procedimentos para status
     public static int mana(){
-        int guarde;
+        int guarde = 0;
         switch (p1.classe) {
             case "Guerreiro":
                 guarde = p1.manaMaxima+4+p1.sabedoria;
@@ -41,4 +81,6 @@ public class Principal {
             default:
                 break;
         }
+        return guarde;
     }
+}
---
+++ LabirintoDasAlmasPerdidas/src/Game/scanner.java
@@ -0,0 +1,9 @@
+
+public class scanner {
+
+    public static Object nextInt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextInt'");
+    }
+
+}
